05faa133ac5d8f696badfbe82f6fba58747fd8bb
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-02-13 21:12:09 +0100

    DATABASE CONNECTION FILE

---
+++ BACKEND/class/DataSource.php
@@ -0,0 +1,115 @@
+<?php
+namespace Usersystem;
+
+class DataSource
+{
+       const HOST = 'localhost'; // Change to Your Own host
+       const USERNAME = 'Your SQL username'; // Change to Your Own Database username
+       const PASSWORD = 'Your SQL userpass'; // Change to Your Own Database password
+       const DATABASENAME = 'Your SQL databasename'; // Change to Your Own Database Name
+	     const USERTABLE = 'Users'; // Change to Your Own Database TABLE Name
+	
+    private $conn;
+
+
+    function __construct()
+    {
+        $this->conn = $this->getConnection();
+    }
+
+
+    public function getConnection()
+    {
+        $conn = new \mysqli(self::HOST, self::USERNAME, self::PASSWORD, self::DATABASENAME);
+        
+        if (mysqli_connect_errno()) {
+            trigger_error("Problem with connecting to database.");
+        }
+        
+        $conn->set_charset("utf8");
+        return $conn;
+    }
+
+
+    public function select($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $resultset[] = $row;
+            }
+        }
+        
+        if (! empty($resultset)) {
+            return $resultset;
+        }
+    }
+    
+
+    public function insert($query, $paramType, $paramArray)
+    {
+        $stmt = $this->conn->prepare($query);
+        $this->bindQueryParams($stmt, $paramType, $paramArray);
+        $stmt->execute();
+        $insertId = $stmt->insert_id;
+        return $insertId;
+    }
+	
+
+	    public function del($query, $paramType, $paramArray)
+    {
+        $stmt = $this->conn->prepare($query);
+		//$stmt->bind_param($paramType, $paramArray);
+        $this->bindQueryParams($stmt, $paramType, $paramArray);
+        $stmt->execute();
+        $delId = $stmt->del_id;
+        return $delId;
+    }
+	
+    
+    public function execute($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType="", $paramArray=array());
+        }
+        $stmt->execute();
+    }
+    
+
+    public function bindQueryParams($stmt, $paramType, $paramArray=array())
+    {
+        $paramValueReference[] = & $paramType;
+        for ($i = 0; $i < count($paramArray); $i ++) {
+            $paramValueReference[] = & $paramArray[$i];
+        }
+        call_user_func_array(array(
+            $stmt,
+            'bind_param'
+        ), $paramValueReference);
+    }
+    
+ 
+    public function numRows($query, $paramType="", $paramArray=array())
+    {
+        $stmt = $this->conn->prepare($query);
+        
+        if(!empty($paramType) && !empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        
+        $stmt->execute();
+        $stmt->store_result();
+        $recordCount = $stmt->num_rows;
+        return $recordCount;
+    }
+}
