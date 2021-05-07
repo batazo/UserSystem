2aea86f7cee1f95ce433265aa012b8d11f4e0658
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-05-07 23:31:41 +0200

    Update public Index.php

--- public/index.php
+++ public/index.php
@@ -4,14 +4,35 @@ use UserSystem\Components\DataSource;
 use UserSystem\Components\Member;
 use UserSystem\Components\Score;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 require_once "../private/vendor/autoload.php";
 
 $dataSource = new DataSource();
 $member = new Member();
 $score = new Score();
 
-dump($dataSource);
-dump($member);
-dump($score);
+//dump($dataSource);
+//dump($member);
+//dump($score);
+
+
+// Instantiate App
+$app = AppFactory::create();
+
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add routes
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('Welcome');
+    return $response;
+});
+
+
+
+$app->run();
 
 exit;
