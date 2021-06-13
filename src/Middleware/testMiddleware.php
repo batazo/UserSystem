431ad10a13c9d3551727933c1c9eddded16cc3ce
bzozoo <47896536+bzozoo@users.noreply.github.com>
2021-06-13 14:00:31 +0200

    Add files via upload

--- src/Middleware/testMiddleware.php
+++ src/Middleware/testMiddleware.php
@@ -3,30 +3,29 @@ namespace UserSystem\Middleware;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface;
 use Slim\Psr7\Response;
 use Slim\Routing\RouteContext;
 
 //Test before middleware
 class testMiddleware
 {
-    public function __invoke(Request $request, RequestHandler $handler)
+    public function __invoke(Request $request, RequestHandler $handler): ResponseInterface
     {
         $response = $handler->handle($request);     
         $existingContent = (string) $response->getBody();
 
-        $routeContext = RouteContext::fromRequest($request);
-        $route = $routeContext->getRoute();     
         
-        $testR = ($existingContent === 'PASSW 2')? 'IGEN ÖN JOGOSULT':'NEM JOGOSULT';
+        if($existingContent === 'PASSW 2'){
         
-        $headers = getallheaders();
-        //dump($headers);
-        $name = $route->getArgument('name');
-        echo $name;   
         $response = new Response();
-        $response->getBody()->write($testR . ' ' . $existingContent);
-        $response->getBody()->write(' ' . $testR);
+        $response->getBody()->write('IGEN ' . $existingContent);
+        $response->getBody()->write(' ÖN JOGOSULT! ');
         
         return $response;
+        
+        }
+        
+        return $handler->handle($request);
     }
 }
